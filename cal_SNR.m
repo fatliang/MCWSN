5c0d924df58ed5c4ff420ae237d6dcf5e20a572d
unknown <liangy11@ECE343.egr.msu.edu>
2015-11-25 16:31:47 -0500

    Routing

--- cal_SNR.m
+++ cal_SNR.m
@@ -5,6 +5,6 @@ pw_r = pw/(dist_next^beta)*exprnd(1);
 intf_ind = (trans_CH ~= trans_ind);
 intf_CH = network(trans_CH(intf_ind));
 dist_intf = cal_dist_pos(intf_CH,network(trans_ind).pos_next(1),network(trans_ind).pos_next(2));
-dist_intf = dist_intf(dist_intf < 4*Rc);
+dist_intf = dist_intf(dist_intf < 8*Rc);
 intf_r = pw./(dist_intf.^beta).*exprnd(1,size(dist_intf));%the interference from other CHs
 res = pw_r/(1+sum(intf_r));
